f731740b79dd786312c751a3e198b1a98435421a
Ademola Sobaki <demolasobaki@gmail.com>
2020-08-08 23:19:41 +0100

    setup graphql server

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -1,1 +1,26 @@
-console.log('hello graphql')
+import { GraphQLServer } from 'graphql-yoga'
+
+// Type definitions (schema)
+const typeDefs = `
+    type Query {
+        hello: String!
+    }
+`
+
+// Resolvers
+const resolvers = {
+	Query: {
+		hello() {
+			return 'Hello World!!!'
+		},
+	},
+}
+
+const server = new GraphQLServer({
+	typeDefs,
+	resolvers,
+})
+
+server.start(() => {
+	console.log('The Server is running')
+})
