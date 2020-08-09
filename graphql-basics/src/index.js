d0739d63b47138fb4a9b4430a82dee3bf5bcd87b
Ademola Sobaki <demolasobaki@gmail.com>
2020-08-09 22:25:01 +0100

    create user and post types

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -3,15 +3,35 @@ import { GraphQLServer } from 'graphql-yoga'
 // Type definitions (schema)
 const typeDefs = `
     type Query {
-        hello: String!
-    }
+		me: User!
+		post: Post!
+	}
+	
+	type User {
+		id: ID!
+		name: String!
+		email: String!
+		age: Int
+	}
+
+	type Post {
+		id: ID!
+		title: String!
+		body: String!
+		published: Boolean!
+	}
 `
 
 // Resolvers
 const resolvers = {
 	Query: {
-		hello() {
-			return 'Hello World!!!'
+		me() {
+			return {
+				id: '123098',
+				name: 'Ademola',
+				email: 'dem@test.com',
+				age: 28,
+			}
 		},
 	},
 }
