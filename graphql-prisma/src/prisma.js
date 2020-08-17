d23d75b81277e09028acb23887449ae7f0cf872f
Ademola Sobaki <demolasobaki@gmail.com>
2020-08-17 09:35:37 +0100

    feat: use prisma bindings with query

--- graphql-prisma/src/prisma.js
+++ graphql-prisma/src/prisma.js
@@ -2,5 +2,19 @@ import { Prisma } from 'prisma-binding'
 
 const prisma = new Prisma({
 	typeDefs: 'src/generated/prisma.graphql',
-	endpoint: 'localhost:4466',
+	endpoint: 'http://localhost:4466',
 })
+
+prisma.query
+	.users(null, '{ id name email posts {id title } }')
+	.then((data) => {
+		console.log(JSON.stringify(data, undefined, 2))
+	})
+	.catch((err) => console.log(err))
+
+prisma.query
+	.comments(null, '{ id text author { id name } }')
+	.then((data) => {
+		console.log(JSON.stringify(data, undefined, 4))
+	})
+	.catch((err) => console.log(err))
