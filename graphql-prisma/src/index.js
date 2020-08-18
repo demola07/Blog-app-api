969f81d4e31e951d4526471ef611921b786de486
Ademola Sobaki <demolasobaki@gmail.com>
2020-08-18 12:40:53 +0100

    feat: use fragments to lockdown individual type fields for only authenticated users

--- graphql-prisma/src/index.js
+++ graphql-prisma/src/index.js
@@ -1,32 +1,20 @@
 import { GraphQLServer, PubSub } from 'graphql-yoga'
 
-import Query from './resolvers/Query'
-import Mutation from './resolvers/Mutation'
-import Subscription from './resolvers/Subscription'
-import User from './resolvers/User'
-import Post from './resolvers/Post'
-import Comment from './resolvers/Comment'
-
+import { resolvers, fragmentReplacements } from './resolvers/index'
 import prisma from './prisma'
 
 const pubsub = new PubSub()
 
 const server = new GraphQLServer({
 	typeDefs: './src/schema.graphql',
-	resolvers: {
-		Query,
-		Mutation,
-		Subscription,
-		User,
-		Post,
-		Comment,
-	},
+	resolvers,
 	context(request) {
 		return {
 			prisma,
 			request,
 		}
 	},
+	fragmentReplacements,
 })
 
 server.start(() => {
