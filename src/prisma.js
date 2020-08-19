ac070e37ccb0411ca438d5860bf2a880785d5089
Ademola Sobaki <demolasobaki@gmail.com>
2020-08-19 13:46:59 +0100

    file and code clean up

---
+++ src/prisma.js
@@ -0,0 +1,12 @@
+import { Prisma } from 'prisma-binding'
+
+import { fragmentReplacements } from './resolvers/index'
+
+const prisma = new Prisma({
+	typeDefs: 'src/generated/prisma.graphql',
+	endpoint: 'http://localhost:4466',
+	secret: 'thisismysupersecretpassword',
+	fragmentReplacements,
+})
+
+export { prisma as default }
