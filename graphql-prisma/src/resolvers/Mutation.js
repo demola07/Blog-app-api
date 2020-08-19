e05269c9744997aa9286647a9d7f140c4ba54f4a
Ademola Sobaki <demolasobaki@gmail.com>
2020-08-19 10:30:40 +0100

    fix: fixed bug in while generating token

--- graphql-prisma/src/resolvers/Mutation.js
+++ graphql-prisma/src/resolvers/Mutation.js
@@ -1,5 +1,4 @@
 import bcrypt from 'bcryptjs'
-import jwt from 'jsonwebtoken'
 
 import getUserId from '../utils/getUserId'
 import generateJwt from '../utils/generateJwt'
--- graphql-prisma/src/utils/generateJwt.js
+++ graphql-prisma/src/utils/generateJwt.js
@@ -1,7 +1,7 @@
 import jwt from 'jsonwebtoken'
 
 const generateJwt = (userId) => {
-	jwt.sign({ userId }, 'thisisasecret', {
+	return jwt.sign({ userId }, 'thisisasecret', {
 		expiresIn: '3 days',
 	})
 }
