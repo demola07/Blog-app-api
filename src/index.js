1b321cba049599ce8aef9d0a61cc7491b84a82fe
Ademola Sobaki <demolasobaki@gmail.com>
2020-08-20 12:16:26 +0100

    feat: config babel poylfill

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
+import '@babel/polyfill'
 import { GraphQLServer, PubSub } from 'graphql-yoga'
 
 import { resolvers, fragmentReplacements } from './resolvers/index'
 import prisma from './prisma'
-
-const pubsub = new PubSub()
 
 const server = new GraphQLServer({
 	typeDefs: './src/schema.graphql',
